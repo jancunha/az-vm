be7a859be709f8c21fef224ab17a80399d44786a
João Alberto <jancunha@gmail.com>
2023-07-09 11:51:23 -0300

    Change terraform version

--- vm.tf
+++ vm.tf
@@ -1,57 +1,57 @@
-resource "tls_private_key" "linux_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# resource "tls_private_key" "linux_key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
-# We want to save the private key to our machine
-# We can then use this key to connect to our Linux VM
+# # We want to save the private key to our machine
+# # We can then use this key to connect to our Linux VM
 
-resource "local_file" "linuxkey" {
-  filename = "azkey.pem"
-  content  = tls_private_key.linux_key.private_key_pem
-}
+# resource "local_file" "linuxkey" {
+#   filename = "azkey.pem"
+#   content  = tls_private_key.linux_key.private_key_pem
+# }
 
-resource "azurerm_network_interface" "this" {
-  name                = "nic"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+# resource "azurerm_network_interface" "this" {
+#   name                = "nic"
+#   location            = azurerm_resource_group.this.location
+#   resource_group_name = azurerm_resource_group.this.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.internal.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
 
-resource "azurerm_linux_virtual_machine" "this" {
-  name                = "app1"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.this.id,
-  ]
+# resource "azurerm_linux_virtual_machine" "this" {
+#   name                = "app1"
+#   resource_group_name = azurerm_resource_group.this.name
+#   location            = azurerm_resource_group.this.location
+#   size                = "Standard_B1s"
+#   admin_username      = "azureuser"
+#   network_interface_ids = [
+#     azurerm_network_interface.this.id,
+#   ]
 
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = tls_private_key.linux_key.public_key_openssh
-  }
+#   admin_ssh_key {
+#     username   = "azureuser"
+#     public_key = tls_private_key.linux_key.public_key_openssh
+#   }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "StandardSSD_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts-gen2"
+#     version   = "latest"
+#   }
 
-  depends_on = [
-    # azurerm_network_interface.app_interface,
-    tls_private_key.linux_key
-  ]
-}
+#   depends_on = [
+#     # azurerm_network_interface.app_interface,
+#     tls_private_key.linux_key
+#   ]
+# }
--- vnet.tf
+++ vnet.tf
@@ -1,20 +1,18 @@
-# Create a resource group
 resource "azurerm_resource_group" "this" {
-  name     = "tcb"
+  name     = "lab"
   location = "East US"
 }
 
-# Create a virtual network within the resource group
 resource "azurerm_virtual_network" "this" {
-  name                = "Bootcamp"
+  name                = "lab-net"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
-  address_space       = ["10.2.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.2.0.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
