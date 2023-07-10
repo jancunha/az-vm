7adb0558d70678f042fbee9b7d2467bc66336c91
João Alberto <jancunha@gmail.com>
2023-07-09 22:18:22 -0300

    Adjust add another VM

--- vm.tf
+++ vm.tf
@@ -3,19 +3,48 @@ data "azurerm_ssh_public_key" "this" {
   resource_group_name = "NHT"
 }
 
-resource "azurerm_linux_virtual_machine" "this" {
-  name                            = "vmlab1"
+resource "azurerm_linux_virtual_machine" "web" {
+  name                            = "vm-web"
   resource_group_name             = azurerm_resource_group.this.name
   location                        = azurerm_resource_group.this.location
   size                            = "Standard_B1s"
   admin_username                  = "azureuser"
   disable_password_authentication = true
   network_interface_ids = [
-    azurerm_network_interface.this.id,
+    azurerm_network_interface.external.id,
   ]
 
   admin_ssh_key {
-    username = "azureuser"
+    username   = "azureuser"
+    public_key = data.azurerm_ssh_public_key.this.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "db" {
+  name                            = "vm-db"
+  resource_group_name             = azurerm_resource_group.this.name
+  location                        = azurerm_resource_group.this.location
+  size                            = "Standard_B1s"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
+  network_interface_ids = [
+    azurerm_network_interface.internal.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
     public_key = data.azurerm_ssh_public_key.this.public_key
   }
 
@@ -33,5 +62,9 @@ resource "azurerm_linux_virtual_machine" "this" {
 }
 
 output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.this.public_ip_address
+  value = azurerm_linux_virtual_machine.web.public_ip_address
+}
+
+output "private_ip_address" {
+  value = azurerm_linux_virtual_machine.db.private_ip_address
 }
--- vnet.tf
+++ vnet.tf
@@ -17,6 +17,13 @@ resource "azurerm_subnet" "external" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "myPublicIP"
@@ -42,23 +49,53 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-resource "azurerm_network_interface" "this" {
-  name                = "nic"
+resource "azurerm_network_interface" "external" {
+  name                = "nic-external"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
   ip_configuration {
-    name                          = "nic-config"
+    name                          = "nic-config-external"
     subnet_id                     = azurerm_subnet.external.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
   }
 }
 
+resource "azurerm_network_interface" "internal" {
+  name                = "nic-internal"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "nic-config-internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    # public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.this.id
+  network_interface_id      = azurerm_network_interface.external.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
+
+resource "azurerm_network_interface_security_group_association" "internal" {
+  network_interface_id      = azurerm_network_interface.internal.id
   network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
